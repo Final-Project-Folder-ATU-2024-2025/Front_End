6294cb58c856efe94b0a195bed4bda99a82e3098
Estevan Fernandez <g00410424@atu.ie>
2024-11-08 14:05:47 +0000

    Completely set front end to communicate with back end

--- collabfy-frontend/src/app/app.component.ts
+++ collabfy-frontend/src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './api.service';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, HttpClientModule],
   template: `
     <h1>Task List</h1>
     <ul>
--- collabfy-frontend/src/app/app.module.ts
+++ collabfy-frontend/src/app/app.module.ts
@@ -1,19 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from './app.component';
 import { ApiService } from './api.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    HttpClientModule  // Ensures that HttpClientModule is available in the app
+    HttpClientModule
   ],
   providers: [ApiService],
-  bootstrap: [AppComponent]
 })
 export class AppModule { }
