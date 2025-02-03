7e04ffe5b4a11a40d04032a4f0799190dbe4f198
Estevan Fernandez <g00410424@atu.ie>
2025-02-03 14:38:42 +0000

    Add styling to the home page

--- collabfy-frontend/src/app/home-page/home-page.component.ts
+++ collabfy-frontend/src/app/home-page/home-page.component.ts
@@ -1,3 +1,7 @@
+// home-page.component.ts
+// This component displays one option at a time from a list of options (e.g., Create a New Project, My Projects, etc.).
+// Users can navigate between options using left and right arrow buttons.
+
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
@@ -6,8 +10,40 @@ import { FooterComponent } from '../footer/footer.component';
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule,HeaderComponent, FooterComponent],
+  // Import shared components and CommonModule for structural directives
+  imports: [CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css'],
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  // List of options to be shown in the slider
+  options: string[] = [
+    'Create a New Project',
+    'My Projects',
+    'Project Deadlines',
+    'Notifications'
+  ];
+
+  // Tracks the currently displayed option's index
+  currentIndex: number = 0;
+
+  // Method to navigate to the previous option in the list.
+  // If at the beginning, it loops to the last option.
+  prev(): void {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    } else {
+      this.currentIndex = this.options.length - 1;
+    }
+  }
+
+  // Method to navigate to the next option in the list.
+  // If at the end, it loops back to the first option.
+  next(): void {
+    if (this.currentIndex < this.options.length - 1) {
+      this.currentIndex++;
+    } else {
+      this.currentIndex = 0;
+    }
+  }
+}
