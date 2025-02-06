882dbb515dcc5ee2c4571b4523c8ecaf80cf5498
Estevan Fernandez <g00410424@atu.ie>
2025-02-06 11:35:38 +0000

    create-project-page generated

---
+++ collabfy-frontend/src/app/create-project-page/create-project-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-project-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-project-page.component.html',
+  styleUrl: './create-project-page.component.css'
+})
+export class CreateProjectPageComponent {
+
+}
