cb39c53db3e22741f8c7a6e55dddaff5d514568b
Estevan Fernandez <g00410424@atu.ie>
2025-02-07 18:19:06 +0000

    Updated main page style

--- collabfy-frontend/src/app/header-public/header-public.component.ts
+++ collabfy-frontend/src/app/header-public/header-public.component.ts
@@ -1,9 +1,7 @@
-// src/app/header-public/header-public.component.ts
-// This header is used on public pages (Login and Create Account).
-// It displays only the logo (with no login/create account or logout buttons).
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { Router, NavigationEnd, RouterLink } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header-public',
@@ -12,4 +10,19 @@ import { RouterLink } from '@angular/router';
   templateUrl: './header-public.component.html',
   styleUrls: ['./header-public.component.css'],
 })
-export class HeaderPublicComponent {}
+export class HeaderPublicComponent implements OnInit {
+  currentUrl: string = '';
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    // Set the current URL initially
+    this.currentUrl = this.router.url;
+    // Subscribe to router events to update currentUrl on navigation
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.currentUrl = event.url;
+    });
+  }
+}
--- collabfy-frontend/src/app/main-page/main-page.component.ts
+++ collabfy-frontend/src/app/main-page/main-page.component.ts
@@ -1,18 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderPublicComponent } from '../header-public/header-public.component';
 import { FooterComponent } from '../footer/footer.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
   imports: [CommonModule, HeaderPublicComponent, FooterComponent, FormsModule, HttpClientModule],
   templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.css'
+  styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent {
-
-}
+export class MainPageComponent { }
