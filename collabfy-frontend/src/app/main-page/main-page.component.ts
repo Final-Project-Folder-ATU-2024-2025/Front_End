e7e361045476fa3d0572a407ef3694123a7c3397
Estevan Fernandez <g00410424@atu.ie>
2025-02-07 17:13:42 +0000

    Changed few styles

--- collabfy-frontend/src/app/main-page/main-page.component.ts
+++ collabfy-frontend/src/app/main-page/main-page.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderPublicComponent } from '../header-public/header-public.component';
+import { FooterComponent } from '../footer/footer.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HeaderPublicComponent, FooterComponent, FormsModule, HttpClientModule],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
