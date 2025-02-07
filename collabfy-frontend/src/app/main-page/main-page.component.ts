69547945f2654a6864b9c7aab803e5657fcc0ab7
Estevan Fernandez <g00410424@atu.ie>
2025-02-07 18:45:41 +0000

    More styling

--- collabfy-frontend/src/app/main-page/main-page.component.ts
+++ collabfy-frontend/src/app/main-page/main-page.component.ts
@@ -4,6 +4,7 @@ import { HeaderPublicComponent } from '../header-public/header-public.component'
 import { FooterComponent } from '../footer/footer.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-page',
@@ -12,4 +13,11 @@ import { HttpClientModule } from '@angular/common/http';
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent { }
+export class MainPageComponent {
+  // Inject Router in the constructor.
+  constructor(private router: Router) {}
+
+  goToCreateAccount(): void {
+    this.router.navigate(['/create-account']);
+  }
+}
