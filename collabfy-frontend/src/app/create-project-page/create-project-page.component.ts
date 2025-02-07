866b098e93a9d584536c67066b1fef75594b868d
Estevan Fernandez <g00410424@atu.ie>
2025-02-07 10:26:44 +0000

    Updated style on create project to fill full screen

--- collabfy-frontend/src/app/create-project-page/create-project-page.component.ts
+++ collabfy-frontend/src/app/create-project-page/create-project-page.component.ts
@@ -1,12 +1,11 @@
 // src/app/create-project-page/create-project-page.component.ts
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-create-project-page',
@@ -16,5 +15,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./create-project-page.component.css']
 })
 export class CreateProjectPageComponent {
-  // Add project creation logic as needed.
+  // Add any project creation logic here.
 }
+
+
