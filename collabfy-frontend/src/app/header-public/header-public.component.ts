f3697f0d07607fb07e7e113c68ad16d2fc3b791b
Estevan Fernandez <g00410424@atu.ie>
2025-02-07 17:29:58 +0000

    Added button to login-page

--- collabfy-frontend/src/app/header-public/header-public.component.ts
+++ collabfy-frontend/src/app/header-public/header-public.component.ts
@@ -3,11 +3,12 @@
 // It displays only the logo (with no login/create account or logout buttons).
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header-public',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './header-public.component.html',
   styleUrls: ['./header-public.component.css'],
 })
