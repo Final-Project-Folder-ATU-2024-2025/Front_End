1d60e802b951da9867b57694d12817a798e467c0
Estevan Fernandez <g00410424@atu.ie>
2025-02-11 12:01:57 +0000

    Created a page for My Projects

---
+++ collabfy-frontend/src/app/my-projects-page/my-projects-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-projects-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-projects-page.component.html',
+  styleUrl: './my-projects-page.component.css'
+})
+export class MyProjectsPageComponent {
+
+}
