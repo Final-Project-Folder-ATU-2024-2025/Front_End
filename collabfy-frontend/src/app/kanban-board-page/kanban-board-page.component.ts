992d1e1bc4622af01f63c18187404d8e4cf5cc9b
Estevan Fernandez <g00410424@atu.ie>
2025-02-21 11:44:13 +0000

    Generated Kanban Board Page

---
+++ collabfy-frontend/src/app/kanban-board-page/kanban-board-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kanban-board-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './kanban-board-page.component.html',
+  styleUrl: './kanban-board-page.component.css'
+})
+export class KanbanBoardPageComponent {
+
+}
