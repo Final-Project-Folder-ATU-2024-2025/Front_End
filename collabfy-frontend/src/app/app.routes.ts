e9f6742d3430bee2f7a32516bb052ef469a7da88
Estevan Fernandez <g00410424@atu.ie>
2025-02-21 11:46:48 +0000

    Updated app.modules

--- collabfy-frontend/src/app/app.routes.ts
+++ collabfy-frontend/src/app/app.routes.ts
@@ -5,12 +5,14 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { CreateProjectPageComponent } from './create-project-page/create-project-page.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { MyProjectsPageComponent } from './my-projects-page/my-projects-page.component';
+import { KanbanBoardPageComponent } from './kanban-board-page/kanban-board-page.component';
 
 export const routes: Routes = [
-  { path: '', component: MainPageComponent },  // Main page route
+  { path: '', component: MainPageComponent },  
   { path: 'home-page', component: HomePageComponent },
   { path: 'create-account', component: CreateAccountPageComponent },
   { path: 'login', component: LoginPageComponent },
   { path: 'create-project-page', component: CreateProjectPageComponent },
-  { path: 'my-projects-page', component: MyProjectsPageComponent }
+  { path: 'my-projects-page', component: MyProjectsPageComponent },
+  { path: 'kanban-board-page', component: KanbanBoardPageComponent }
 ];
