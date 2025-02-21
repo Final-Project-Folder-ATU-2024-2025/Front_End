e89a9cae6cf1bdbffc1261b30b60292e49ea91d9
Estevan Fernandez <g00410424@atu.ie>
2025-02-21 11:52:27 +0000

    Added some style to Kanvban Page.

--- collabfy-frontend/src/app/kanban-board-page/kanban-board-page.component.ts
+++ collabfy-frontend/src/app/kanban-board-page/kanban-board-page.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-kanban-board-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HeaderComponent, FooterComponent, FormsModule, HttpClientModule],
   templateUrl: './kanban-board-page.component.html',
-  styleUrl: './kanban-board-page.component.css'
+  styleUrls: ['./kanban-board-page.component.css']
 })
-export class KanbanBoardPageComponent {
-
-}
+export class KanbanBoardPageComponent {}
