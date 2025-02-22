a1d5215507f4ca5b8f27a566468350d7eeb35fa6
Estevan Fernandez <g00410424@atu.ie>
2025-02-22 13:20:29 +0000

    Fixing more errors.

--- collabfy-frontend/src/app/api.service.spec.ts
+++ collabfy-frontend/src/app/api.service.spec.ts
@@ -4,10 +4,10 @@ import { ApiService } from './api.service';
 describe('ApiService', () => {
   let service: ApiService;
 
-  beforeEach(() => {
-annot find name 'befor    TestBed.configureTestingModule({});
-    service = TestBed.inject(ApiService);
-  });
+beforeEach(() => {
+  TestBed.configureTestingModule({});
+  service = TestBed.inject(ApiService);
+});
 
   it('should be created', () => {
     expect(service).toBeTruthy();
