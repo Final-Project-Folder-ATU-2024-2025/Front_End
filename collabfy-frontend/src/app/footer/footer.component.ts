230e7862111a0cce2b3124d516206291aa99dd24
Estevan Fernandez <g00410424@atu.ie>
2025-04-03 10:57:33 +0100

    Polished footer component

--- collabfy-frontend/src/app/footer/footer.component.ts
+++ collabfy-frontend/src/app/footer/footer.component.ts
@@ -1,11 +1,19 @@
+// =======================================================================
+// Import necessary Angular modules
+// =======================================================================
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// =======================================================================
+// FooterComponent:
+// This component displays the footer content including copyright
+// information and a link to the GitHub profile.
+// =======================================================================
 @Component({
-    selector: 'app-footer',
-    imports: [CommonModule],
-    templateUrl: './footer.component.html',
-    styleUrls: ['./footer.component.css'],
-    standalone: true
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {}
